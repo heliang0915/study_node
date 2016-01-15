160779955f72b7958fadf7e1cfd718fe5affa074
heliang <598481337@qq.com>
2016-01-15 16:15:22 +0800

    去掉多余注释

--- lesson_2/app.js
+++ lesson_2/app.js
@@ -3,46 +3,20 @@
  */
 var http = require("http");
 var fs = require("fs");
-
 var mime=require("mime");
-
-
 var rootDir="./view/";
 var onRequest = function (req, res) {
-
     var url=req.url;
-
-    console.log(url);
-    //
-    //if(url=="/"){
-    //    render(res, rootDir+"index.html", function (err, data) {
-    //        var mes=(err==null?data:"<font style='color:red'>程序出现错误,错误原因:"+err.toString()+"</font>");
-    //        print(res, mes);
-    //    })
-    //
-    //}else if(url=="/style.css"){
-    //    render(res, rootDir+"style.css", function (err, data) {
-    //        var mes=(err==null?data:"<font style='color:red'>程序出现错误,错误原因:"+err.toString()+"</font>");
-    //        print(res, mes,"css");
-    //    })
-    //}
     static(url,res);
 
-
-
 }
-
 function static(url,res){
-
-    console.log(mime.lookup(url));
     var type="";
     if(url=="/"){
         type="text/html";
     }else{
         type=mime.lookup(url);
     }
-
-    console.log("type>>"+type);
     res.writeHead(200, {
         'Content-Type': type+';charset=utf-8'
     });
@@ -56,22 +30,7 @@ function static(url,res){
         res.write(mes);
         res.end();
     });
-
-    //render(res, rootDir+"index.html", function (err, data) {
-    //    var mes=(err==null?data:"<font style='color:red'>程序出现错误,错误原因:"+err.toString()+"</font>");
-    //
-    //    res.writeHead(200, {
-    //        'Content-Type': 'text/'+mime.lookup(url)+';charset=utf-8'
-    //    });
-    //    res.write(mes);
-    //    res.end();
-    //
-    //})
 }
-
-
-
-
 var server = http.createServer(onRequest);
 server.listen(8022, function () {
     console.log("服务器程序已经启动...");
