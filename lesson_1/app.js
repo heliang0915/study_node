44ec84a4a5a86bae546df6cc35922c3ced5f085f
heliang <598481337@qq.com>
2016-01-15 15:15:06 +0800

    添加部分资源

--- lesson_1/app.js
+++ lesson_1/app.js
@@ -1,15 +1,33 @@
 /**
  * Created by lenovo on 2016/1/15.
  */
-var http=require("http");
-var onRequest=function(req,res){
-    res.writeHead(200,{
-        'Content-Type':'text/html;charset=utf-8'
+var http = require("http");
+var fs = require("fs");
+var onRequest = function (req, res) {
+    render(res, "./view/index.html", function (err, data) {
+        console.log(JSON.stringify(err));
+        var mes=(err==null?data:"<font style='color:red'>程序出现错误,错误原因:"+err.toString()+"</font>");
+        print(res, mes);
+    })
+    //res.write("第一个 Node程序");
+    //
+    //res.end();
+}
+var render = function (res, html, callback) {
+    fs.readFile(html, function (err, data) {
+        if (callback) {
+            callback(err, data);
+        }
     });
-    res.write("第一个 Node程序");
+}
+var print = function (res, data) {
+    res.writeHead(200, {
+        'Content-Type': 'text/html;charset=utf-8'
+    });
+    res.write(data);
     res.end();
 }
-var server=http.createServer(onRequest);
-server.listen(8022,function(){
+var server = http.createServer(onRequest);
+server.listen(8022, function () {
     console.log("服务器程序已经启动...");
 });
